8b21e98ad812b84925c6c531194d897cb133d1b9
jaga24-dev <jugo_24@alu.uabcs.mx>
2026-02-25 18:50:10 -0700

    creacion Registro

--- src/ventana/VentanaLogin.java
+++ src/ventana/VentanaLogin.java
@@ -5,19 +5,24 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class VentanaLogin extends JFrame{
 	public VentanaLogin() {
 		// Ventana 
-		this.setSize(600, 600);
+		this.setSize(1200, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setMinimumSize(new Dimension(200,200));
 		this.setMaximumSize(new Dimension(800,800));
@@ -90,18 +95,110 @@ public class VentanaLogin extends JFrame{
 		acceder.setBounds(100, 340, 300, 50);
 		acceder.setFont(new Font("Arial", Font.BOLD, 22));
 		acceder.setForeground(Color.white);
-		acceder.setBackground(Color.blue);
+		acceder.setBackground(Color.decode("#6A5ACD"));
 		contenedor.add(acceder);
 		
-		// Acción del botón
-		acceder.addActionListener(e -> {
-			String user = username.getText();
-			String pass = new String(password.getPassword());
-			boolean recordarUsuario = recordar.isSelected();
-			System.out.println("Usuario: " + user);
-			System.out.println("Contraseña: " + pass);
-			System.out.println("Recordar: " + recordarUsuario);
-		});
+		
+		//contenedor registro
+		JPanel register_container = new JPanel();
+		register_container.setBounds(50, 50, 500, 500);
+		register_container.setLocation(600, 50);
+		register_container.setOpaque(true);
+		register_container.setBackground(Color.decode("#27F5D6"));
+		register_container.setLayout(null);
+		this.add(register_container);
+		
+		// Título 
+		JLabel titulo = new JLabel("REGISTRO", JLabel.CENTER);
+		titulo.setBounds(100, 10, 300, 40);
+		titulo.setFont(new Font("Arial", Font.BOLD, 28));
+		titulo.setForeground(Color.BLACK); register_container.add(titulo);
+		
+		// Nombre de usuario 
+		JLabel user_tag = new JLabel("NOMBRE DE USUARIO:"); 
+		user_tag.setBounds(100, 60, 250, 30); 
+		user_tag.setFont(new Font("Arial", Font.BOLD, 18)); 
+		register_container.add(user_tag);
+
+		JTextField new_user = new JTextField("Ingresa tu nombre de usuario"); 
+		new_user.setBounds(100, 90, 300, 30);
+		new_user.setFont(new Font("Arial", Font.PLAIN, 14));
+		new_user.setForeground(Color.GRAY);
+		register_container.add(new_user);
+
+		// BIO 
+		JLabel bio_tag = new JLabel("BIO"); 
+		bio_tag.setBounds(100, 130, 250, 30); 
+		bio_tag.setFont(new Font("Arial", Font.BOLD, 18)); 
+		register_container.add(bio_tag); 
+		
+		JTextArea bio = new JTextArea("Cuéntanos sobre ti..."); 
+		bio.setBounds(100, 160, 300, 100);
+		bio.setFont(new Font("Arial", Font.PLAIN, 14)); 
+		bio.setForeground(Color.GRAY); 
+		bio.setBorder(BorderFactory.createLineBorder(Color.BLACK)); 
+		register_container.add(bio);
+		
+		// Preferencias 
+		JLabel pref_tag = new JLabel("PREFERENCIAS", JLabel.CENTER); 
+		pref_tag.setBounds(100, 270, 250, 30); 
+		pref_tag.setFont(new Font("Arial", Font.BOLD, 18)); 
+		register_container.add(pref_tag); 
+		
+		JCheckBox sweet_option = new JCheckBox("Dulces"); 
+		sweet_option.setBounds(100, 300, 100, 30); 
+		sweet_option.setBackground(Color.decode("#27F5D6")); 
+		register_container.add(sweet_option);
+		
+		JCheckBox salty_option = new JCheckBox("Salado"); 
+		salty_option.setBounds(200, 300, 100, 30); 
+		salty_option.setBackground(Color.decode("#27F5D6")); 
+		register_container.add(salty_option); 
+		
+		JCheckBox healthy = new JCheckBox("Saludable"); 
+		healthy.setBounds(300, 300, 120, 30); 
+		healthy.setBackground(Color.decode("#27F5D6")); 
+		register_container.add(healthy);
+		
+		sweet_option.setBackground(Color.decode("#27F5D6"));
+		salty_option.setBackground(Color.decode("#27F5D6"));
+		healthy.setBackground(Color.decode("#27F5D6"));
+		
+		// Términos 
+		JLabel terms_tag = new JLabel("TÉRMINOS", JLabel.CENTER); 
+		terms_tag.setBounds(100, 340, 250, 30); 
+		terms_tag.setFont(new Font("Arial", Font.BOLD, 18)); 
+		register_container.add(terms_tag); 
+		
+		JRadioButton accept_terms = new JRadioButton("Acepto los términos"); 
+		accept_terms.setBounds(100, 370, 150, 30); 
+		accept_terms.setBackground(Color.decode("#27F5D6")); 
+		register_container.add(accept_terms);
+		
+		JRadioButton reject_terms = new JRadioButton("Rechazo los términos");
+		reject_terms.setBounds(250, 370, 150, 30); 
+		reject_terms.setBackground(Color.decode("#27F5D6")); 
+		register_container.add(reject_terms);
+		
+		ButtonGroup terms=new ButtonGroup();
+		accept_terms.setBackground(Color.decode("#27F5D6"));
+		reject_terms.setBackground(Color.decode("#27F5D6"));
+		terms.add(accept_terms);
+		terms.add(reject_terms);
+		
+		// Dropdown de colonias 
+		String[] colonias = {"Camino Real", "La Fuente", "Villas"}; 
+		JComboBox<String> list = new JComboBox<>(colonias); 
+		list.setBounds(150, 400, 200, 30); 
+		register_container.add(list); 
+		
+		// Botón 
+		JButton crearCuenta = new JButton("Crear cuenta"); 
+		crearCuenta.setBounds(150, 440, 200, 40);
+		crearCuenta.setFont(new Font("Arial", Font.BOLD, 20));
+		crearCuenta.setBackground(Color.YELLOW); 
+		crearCuenta.setForeground(Color.BLACK); 
+		register_container.add(crearCuenta);
 		
 		// Mostrar ventana al final 
 		this.setVisible(true);
